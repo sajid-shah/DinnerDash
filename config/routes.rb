e41012b206c08d58a1fd5c272df2858c7529a5ab
Sajid Shah <sajid.shah@devsinc.com>
2022-07-18 17:52:22 +0500

    Generate User Model
    - generate user with devise with additional fields like fullname, displayname, role
    
    - added role enum in user.rb
    
    - run db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   root 'home#index'
   get 'home/about'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
