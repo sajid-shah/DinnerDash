7cd0893ee6a39eaa86bb9f7506cf1ffcc01a7b55
Sajid Shah <sajid.shah@devsinc.com>
2022-08-02 00:04:27 +0500

    Route file overridden with user instead of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   resources :items
+
   get 'home/index'
   root 'home#index'
   get 'home/about'
+
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords'}
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
