145dfb744994e1df611f6fcb93fe456d8230d61a
Sajid Shah <sajid.shah@devsinc.com>
2022-08-01 15:45:18 +0500

    Update Item Controller
    - added action methods in item controller and its views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
