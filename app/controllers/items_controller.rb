55d752112daf3c2429e814480fa9f01a910fee70
Sajid Shah <sajid.shah@devsinc.com>
2022-08-04 12:32:45 +0500

    Update Header and Item Management View
    - update header for different view roles(guest/admin/customer)
    
    - remove extra style-sheets
    
    - update button style (bootstrap)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,6 +55,6 @@ class ItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def item_params
-    params.require(:item).permit(:title, :description, :price, :active)
+    params.require(:item).permit(:id, :title, :description, :price, :active)
   end
 end
