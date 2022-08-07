47353d29b01a0780eb3af0c21faa5f7252c418a0
Sajid Shah <sajid.shah@devsinc.com>
2022-08-07 23:28:59 +0500

    Authenticate User for actions
    - on delete,update,edit , ask user to sign in
    
    - added user authentications in category and item management

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@
 
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   def index
     @items = Item.all
